0fe48d16929990a24dacd9130f4e871ae10d11d1
chandani.thacker <chandani.thacker@attuneww.com>
2016-06-28 13:18:01 +0530

    first commit

---
+++ src/main/java/DemoFile.java
@@ -0,0 +1,13 @@
+
+public class DemoFile {
+
+	public static void main(String a[])
+	{
+		System.out.println("Returned value is  :  "+returnCalValue());
+	}
+	public static int returnCalValue()
+	{
+		return 10;
+	}
+
+}
