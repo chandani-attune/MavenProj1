8201fd7cc5b50e251d6d255dbf31f7e4e66859ff
chandani.thacker <chandani.thacker@attuneww.com>
2016-06-28 13:27:23 +0530

    second Commit

--- src/main/java/DemoFile.java
+++ src/main/java/DemoFile.java
@@ -3,9 +3,14 @@ public class DemoFile {
 
 	public static void main(String a[])
 	{
-		System.out.println("Returned value is  :  "+returnCalValue());
+		System.out.println("Returned value is  :  "+returnCalValue() + "   "+returnCalValue1());
 	}
 	public static int returnCalValue()
+	{
+		return 10;
+	}
+	
+	public static int returnCalValue1()
 	{
 		return 10;
 	}
